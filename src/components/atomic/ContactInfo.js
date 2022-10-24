6260fdc425b64284dc28526c8dc84b8d854c88da
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-24 15:37:21 -0700

    Add modular css to contact info component

--- src/components/atomic/ContactInfo.js
+++ src/components/atomic/ContactInfo.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Input from "./Input";
-import "./styles/ContactInfo.css";
-
+import styles from "./styles/ContactInfo.module.css";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 export default class ContactInfo extends Component {
   render() {
     return (
-      <div>
-        <Input className="omo">Linkedin Username</Input>
+      <div className={styles.contact}>
+        <LinkedInIcon />
+        <Input>Linkedin Username</Input>
       </div>
     );
   }

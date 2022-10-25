17c7927604e854dd099457423996e6399f164189
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-24 17:05:38 -0700

    Add complete sidebar markup

--- src/components/section/Sidebar.js
+++ src/components/section/Sidebar.js
@@ -2,18 +2,42 @@ import React, { Component } from "react";
 import ContactInfo from "../atomic/ContactInfo";
 import styles from "./styles/Sidebar.module.css";
 import { Facebook } from "@mui/icons-material";
+import EducationInfo from "../atomic/EducationInfo";
+import Input from "../atomic/Input";
 export class Sidebar extends Component {
   render() {
     return (
       <>
         <aside>
-          <h2>Contact</h2>
-          <div className="contactInfo__section">
-            <ContactInfo svg={<Facebook />} />
-            <ContactInfo svg={<Facebook />} />
-            <ContactInfo svg={<Facebook />} />
-            <ContactInfo svg={<Facebook />} />
-            <ContactInfo svg={<Facebook />} />
+          <div className="contact-card">
+            <h2>Contact</h2>
+            <div className={styles.contactInfo__section}>
+              <ContactInfo svg={<Facebook />} />
+              <ContactInfo svg={<Facebook />} />
+              <ContactInfo svg={<Facebook />} />
+              <ContactInfo svg={<Facebook />} />
+              <ContactInfo svg={<Facebook />} />
+            </div>
+          </div>
+          <div className="skills-card">
+            <h2>Skills</h2>
+            <div className={styles.contactInfo__section}>
+              <Input>
+                <p>Reactjs, Typescript, Nodejs, python, HTML, CSS, Next.js</p>
+              </Input>
+            </div>
+          </div>
+          <div className="Education-card">
+            <h2>Education</h2>
+            <div className={styles.contactInfo__section}>
+              <EducationInfo />
+              <EducationInfo />
+              <EducationInfo />
+            </div>
+          </div>
+          <div className="References-card">
+            <h2>References</h2>
+            <p>Add References here</p>
           </div>
         </aside>
       </>

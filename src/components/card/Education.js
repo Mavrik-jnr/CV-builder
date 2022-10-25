56f1dd3eb992162603796047c5995da436ca7c87
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-25 09:59:58 +0100

    Add Sidebar cards

---
+++ src/components/card/Education.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import styles from "./styles/education.module.css";
+
+export default class Education extends Component {
+  render() {
+    return <div>Education</div>;
+  }
+}
---
+++ src/components/card/Skills.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import styles from "./styles/skills.module.css";
+
+export default class Skills extends Component {
+  render() {
+    return <div>Skills</div>;
+  }
+}

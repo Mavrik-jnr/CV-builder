9253d9f120a2c8bfc4471c32ae802ebb2ecb3ad5
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-25 16:08:09 +0100

    Add Array and Conditional logic to renderingsidebar

--- src/components/section/Sidebar.js
+++ src/components/section/Sidebar.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 
-import Contact from "../card/Contact";
-import Skills from "../card/Skills";
-import Education from "../card/Education";
-import References from "../card/References";
 import styles from "./styles/Sidebar.module.css";
+import SidebarComponent from "../utility/SidebarComponent";
 export class Sidebar extends Component {
   render() {
     return (
       <>
         <aside>
-          <Contact />
-          <Skills />
-          <Education />
-          <References />
+          <SidebarComponent />
         </aside>
       </>
     );
--- src/components/utility/SidebarComponent.js
+++ src/components/utility/SidebarComponent.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import Contact from "../card/Contact";
+import Education from "../card/Education";
+import References from "../card/References";
+import Skills from "../card/Skills";
 import styles from "./styles/sidebarcomponent.module.css";
 
 export default class SidebarComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: ["contact", "skills", "education", "references"],
+    };
+    this.setCard = this.setCard.bind(this);
+  }
+
+  setCard(card) {
+    //setting logic
+    if (this.state.cards.includes(card)) {
+      return;
+    } else {
+      this.setState({
+        cards: this.state.cards.append(card),
+      });
+    }
+  }
+  removeCard(card) {
+    this.setState({
+      cards: this.state.cards.filter((element) => element !== card),
+    });
+  }
   render() {
-    return <div>SidebarComponent</div>;
+    return (
+      <>
+        {this.state.cards.map((element) => {
+          if (element === "contact") {
+            return <Contact />;
+          } else if (element === "skills") {
+            return <Skills />;
+          } else if (element === "education") {
+            return <Education />;
+          } else if (element === "references") {
+            return <References />;
+          } else {
+            return undefined;
+          }
+        })}
+      </>
+    );
   }
 }
